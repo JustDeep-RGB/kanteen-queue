9b9d625e33fdf1b48d6275010b9862d10cae0bdd
JustDeep-RGB <jasdeepsinghzzz50@gmail.com>
2026-03-20 01:13:28 +0530

    attempt 2

--- backend/server.js
+++ backend/server.js
@@ -8,8 +8,8 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 app.use('/api', routes);
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/smart-canteen', {
-})
+console.log("MONGO URI:", process.env.MONGODB_URI);
+mongoose.connect(process.env.MONGODB_URI, {})
   .then(() => console.log('Connected to MongoDB'))
   .catch(err => console.error('Failed to connect to MongoDB', err));
 app.listen(PORT, () => {
